3036e34776f6795f1c8a3028fa0d8ed6ec417896
Chris Robinson <awscrob@amazon.com>
2023-07-26 23:04:16 -0700

    Updates

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,10 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { App } from './App';
 import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
+const queryClient = new QueryClient();
 
 
 root.render(
