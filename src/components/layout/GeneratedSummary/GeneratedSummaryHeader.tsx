15ee9899d8da84c5fda2f913225c4e1f1ba826a1
Chris Robinson <awscrob@amazon.com>
2023-08-03 13:40:25 -0700

    Add disclaimer

--- src/components/layout/GeneratedSummary/GeneratedSummaryHeader.tsx
+++ src/components/layout/GeneratedSummary/GeneratedSummaryHeader.tsx
@@ -12,9 +12,10 @@ export const GeneratedSummaryHeader = () => {
     return (
         <>
             <Flex alignItems={'center'} justifyContent={'space-between'}>
-                <Box><Text pb="10" fontSize='4xl' color='black'>Generated messages</Text></Box>
+                <Box><Text pb="0" fontSize='4xl' color='black'>Generated messages</Text></Box>
                 <Box><Button colorScheme='blue'>Download All &nbsp;<DownloadIcon/> </Button></Box>
-            </Flex>          
+            </Flex>  
+            <Box><Text as='i' pb="10" fontSize='xs' color='red'>Disclaimer: This is an early phase prototype and generated messages are not necessarily reflective of end state.</Text></Box>        
         </>
     );
 };
